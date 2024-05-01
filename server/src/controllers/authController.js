10ac1d0191a66915fd4a85c4f49cbed675b563b6
Naseem Khan <naseemsaffy@gmail.com>
2024-05-01 23:12:09 +0500

    Add user registration functionality

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,30 @@
+import handleError from "../middlewares/handleError.js";
+import { User } from "../schema/userModal.js";
+import bcrypt from "bcrypt";
+
 export const registerUser = async (req, res, next) => {
   try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password)
+      return next(handleError(400, "All fields are required!"));
+
+    const user = await User.findOne({ email });
+    if (user) return next(handleError(400, "User already exists!"));
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const { password: userPassword, ...userInfo } = newUser._doc;
+
+    res
+      .status(201)
+      .json({ message: "User Created Successfully!", user: userInfo });
   } catch (error) {
     return next(error.message);
   }
