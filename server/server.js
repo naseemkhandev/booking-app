65c5401a0c267b70480b39dc14f6415a31cb204d
Naseem Khan <naseemsaffy@gmail.com>
2024-05-01 21:26:42 +0500

    Add MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import app from "./src/app.js";
 import { config } from "./src/config/config.js";
+import connectDB from "./src/config/db.js";
 
 const startServer = async () => {
   try {
+    await connectDB();
+
     const port = config.port || 3000;
     app.listen(port, () => console.log(`Server is running on port ${port}`));
   } catch (error) {
--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const _config = {
   port: process.env.PORT,
+  backendUrl: process.env.MONGO_URI,
+  env: process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config);
