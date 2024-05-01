8fd4bf081e8f7eb8af843b28da5907400a289dd3
Naseem Khan <naseemsaffy@gmail.com>
2024-05-01 21:38:59 +0500

    Add authentication route and controller

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
+import authRoute from "./routes/authRoute.js";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/auth", authRoute);
 
 export default app;
