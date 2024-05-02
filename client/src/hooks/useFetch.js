a3c8ec7a05682a8069a412017437b654f863d069
Naseem Khan <naseemsaffy@gmail.com>
2024-05-02 11:21:35 +0500

    add custom hook for fetching data

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (endpoint) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_BACKEND_URL}${endpoint}`
+        );
+        const data = await response.json();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        setError("Error fetching data: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [endpoint]);
+
+  const reFetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_BACKEND_URL}${endpoint}`
+      );
+      const data = await response.json();
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      setError("Error fetching data: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, reFetchData };
+};
+
+export default useFetch;
